b2b2bc85655e02149383dc0a970823dccc0154bf
William Fournier <wfournier2710@gmail.com>
2024-05-18 21:57:39 -0400

    Axios post (create)

--- assets/contexts/TodoContext.js
+++ assets/contexts/TodoContext.js
@@ -15,10 +15,16 @@ class TodoContextProvider extends Component {
     // create
     createTodo(event, todo) {
         event.preventDefault();
-        let data = [...this.state.todos];
-        data.push(todo);
-        this.setState({
-            todos: data,
+        axios.post('/api/todo/create', todo)
+            .then(response => {
+                console.log(response.data);
+                let data = [...this.state.todos];
+                data.push(response.data.todo);
+                this.setState({
+                    todos: data,
+                })
+            }).catch(error => {
+            console.error(error);
         })
     }
 
--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,25 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
 use App\Repository\TodoRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/todo', name: 'app_todo')]
+#[Route('/api/todo', name: 'api_todo')]
 class TodoController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     private TodoRepository $todoRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, TodoRepository $todoRepository) {
+    public function __construct(EntityManagerInterface $entityManager, TodoRepository $todoRepository)
+    {
 
         $this->entityManager = $entityManager;
         $this->todoRepository = $todoRepository;
     }
 
-    #[Route('/read', name: 'app_todo')]
-    public function index()
+    #[Route('/read', name: 'api_todo_read')]
+    public function index(): JsonResponse
     {
         $todos = $this->todoRepository->findAll();
 
@@ -30,5 +35,26 @@ class TodoController extends AbstractController
         }
 
         return $this->json($arrayOfTodos);
+    }
+
+    #[Route('/create', name: 'app_todo_create')]
+    public function create(Request $request): JsonResponse
+    {
+        $content = json_decode($request->getContent());
+
+        $todo = new Todo();
+        $todo->setName($content->name);
+        try {
+            $this->entityManager->persist($todo);
+            $this->entityManager->flush();
+
+            return $this->json([
+                'todo' => $todo->toArray(),
+            ]);
+        } catch (Exception $exception) {
+            return $this->json([
+                'error' => $exception->getMessage(),
+            ]);
+        }
     }
 }
